5c9e13a160b185ed5ced71d7e8f00ec59696f3d4
BMscis <melvinwafula@gmail.com>
2020-04-02 00:02:45 +0300

    malgo says

--- malgo.py
+++ malgo.py
@@ -2,35 +2,29 @@ import re
 import math
 import json
 import decimal
+import asyncio
 import warnings
 import pandas as pd
 import MetaTrader5 as mt
 from datetime import datetime
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-class malgo:
-
-    def __init__(self, lot, type, base, volume, exchange_rate,
-        new_exchange_rate, leverage, balance,target,swap,time_base):
-
+class malgogram:
+    def __init__(self, lot, type, base, volume, exchange_rate,new_exchange_rate, leverage, balance,target,swap,time_base):
         mt.initialize()
+        self.volt = {"time":time_base,'base':base,'price_per_pip_change':None,
+            'ERD':None,'us_base':None, 'bid':None,'ask':None,'profit':None,
+            'swap':round(swap,3),'rate_of_currency_change':None}
+        self.fund = {"target":target,"target_rate":None}
         self.exchange_rate = exchange_rate
         self.new_exchange_rate = new_exchange_rate
-        self.time = time_base
         self.volume = volume
-        self.base = base
-        
-        self.swap = round(swap,3)
         self.type = type
         self.set_lot(lot)
         self.set_expo()
-
         self.calculate_pip_value()
-
         self.set_ROC()
-        self.set_ERD(target)
-
-        
+        self.set_ERD()   
         self.get_profit()
         self.get_margin(leverage,balance)
         self.send_data()
@@ -48,113 +42,116 @@ class malgo:
             self.exponent = self.exponent + 1
         elif self.exponent > -4:
             self.exponent = self.exponent + 1
-    def set_ROC(self):
-        self.rate_of_currency_change = round(self.exchange_rate/self.new_exchange_rate,5)
-    def set_ERD(self,target):
-        target_exchange = (target * pow(10,self.exponent)) / self.price_per_pip_change
-        if self.type == "sell":
-            self.exchange_rate_difference = round(self.exchange_rate\
-                - self.new_exchange_rate,5)
-            self.target_exchange = target_exchange / self.rate_of_currency_change
-            self.target_exchange = round((self.target_exchange - self.exchange_rate) / -1,5)
-        elif self.type == "buy":
-            self.exchange_rate_difference = round(self.new_exchange_rate\
-                - self.exchange_rate,5)
-            self.target_exchange = round((target_exchange / self.rate_of_currency_change)\
-                + self.exchange_rate,5)
     def calculate_pip_value(self):
         one_pip = math.pow(10, self.exponent)
         pip_change = one_pip/self.exchange_rate
-        self.price_per_pip_change = round(pip_change * self.lot_size,8)
+        self.volt['price_per_pip_change'] = round(pip_change * self.lot_size,8)
+    def set_ROC(self):
+        self.volt['rate_of_currency_change'] = round(self.exchange_rate/self.new_exchange_rate,5)
+    def set_ERD(self):
+        target_exchange = (self.fund['target'] * pow(10,self.exponent)) / self.volt['price_per_pip_change']
+        if self.type == "sell":
+            self.volt['ERD'] = round(
+                self.exchange_rate - self.new_exchange_rate,5)
+            self.fund['target_rate'] = target_exchange / self.volt['rate_of_currency_change']
+            self.fund['target_rate'] = round((self.fund['target_rate'] - self.exchange_rate) / -1,5)
+        elif self.type == "buy":
+            self.volt['ERD'] = round(self.new_exchange_rate\
+                - self.exchange_rate,5)
+            self.fund['target_rate'] = round((target_exchange / self.volt['rate_of_currency_change'])\
+                + self.exchange_rate,5)
     def get_profit(self):
         get_rates = None
-        if self.base[:3].lower() != "usd":
+        if self.volt['base'][:3].lower() != "usd":
             try:
-                self.us_base = self.base[:3].upper() + "USD"
-                get_rates = mt.copy_rates_from(self.us_base,
-                    mt.TIMEFRAME_M1,self.time,10)
+                self.volt['us_base'] = self.volt['base'][:3].upper() + "USD"
+                get_rates = mt.copy_rates_from(self.volt['us_base'],
+                    mt.TIMEFRAME_M1,self.volt['time'],10)
                 if get_rates == None:
-                    self.us_base = "USD" + self.base[:3].upper() 
-                    get_rates = mt.copy_rates_from(self.us_base,
-                        mt.TIMEFRAME_M1,self.time,10)
+                    self.volt['us_base'] = "USD" + self.volt['base'][:3].upper() 
+                    get_rates = mt.copy_rates_from(self.volt['us_base'],
+                        mt.TIMEFRAME_M1,self.volt['time'],10)
                     if get_rates == None:
-                        self.bid = 1.0
-                        self.ask = 1.0
+                        self.volt['bid'] = 1.0
+                        self.volt['ask'] = 1.0
             except Exception as e:
-                print(self.base,self.us_base,e)
+                print(self.volt['base'],self.volt['us_base'],e)
         """
-        elif self.base[:3].lower() == "usd":
+        elif self.volt['base'][:3].lower() == "usd":
             try:
-                self.us_base = self.base[3:6].upper() + "USD"
-                get_rates = mt.copy_rates_from(self.us_base,
+                self.volt['us_base'] = self.volt['base'][3:6].upper() + "USD"
+                get_rates = mt.copy_rates_from(self.volt['us_base'],
                     mt.TIMEFRAME_M1,self.time,10)
                 if get_rates == None:
-                    self.us_base = "USD" + self.base[3:6].upper()
-                    get_rates = mt.copy_rates_from(self.us_base,
+                    self.volt['us_base'] = "USD" + self.volt['base'][3:6].upper()
+                    get_rates = mt.copy_rates_from(self.volt['us_base'],
                         mt.TIMEFRAME_M1,self.time,10)
                     if get_rates == None:
-                        self.bid = 1.0
-                        self.ask = 1.0
+                        self.volt['bid'] = 1.0
+                        self.volt['ask'] = 1.0
             
             except Exception as e:
-                print(self.base,self.us_base,e)
+                print(self.volt['base'],self.volt['us_base'],e)
             """
-        get_currency = round(self.exchange_rate_difference\
-            * self.price_per_pip_change,8)
+        get_currency = round(self.volt['ERD']\
+            * self.volt['price_per_pip_change'],8)
         bare_currency = get_currency * pow(10, abs(self.exponent))
         if get_rates != None:
-            self.bid = get_rates[0][1]
-            self.ask = get_rates[0][3]
-            self.profit = round(bare_currency * self.ask,2)
-            self.profit = self.profit + self.swap
-            round(self.profit,2)
+            self.volt['bid'] = get_rates[0][1]
+            self.volt['ask'] = get_rates[0][3]
+            self.volt['profit'] = round(bare_currency * self.volt['ask'],2)
+            self.volt['profit'] = self.volt['profit'] + self.volt['swap']
+            round(self.volt['profit'],2)
         else:
-            self.profit = bare_currency + self.swap
-            self.profit = round(self.profit,2)
+            self.volt['profit'] = bare_currency + self.volt['swap']
+            self.volt['profit'] = round(self.volt['profit'],2)
     def get_margin(self,leverage,balance):
         margin = self.lot_size * self.exchange_rate/leverage
-        equity = balance + self.profit
+        equity = balance + self.volt['profit']
         #free_margin = equity - margin
         #margin_level = equity/margin * 100
     def get_self(self):
         data = {"Exchange rate":self.exchange_rate,"Exponent":self.exponent,
-        "New Exchange":self.new_exchange_rate,"Volume":self.volume,"Lot ":self.lot_size,"Profits":self.profit}
+        "New Exchange":self.new_exchange_rate,"Volume":self.volume,"Lot ":self.lot_size,"Profits":self.volt['profit']}
         return data
     def send_data(self):
         data = self.get_self()
         with open('c:/Users/melvi/git/malgo/malgo_compare.json','a+') as ch:
             json.dump(data,ch)
             ch.write('\n')
-#cargo = malgo("std", "sell", "eur", 0.12, 1.10370, 1.09715, 500, 50,100)
 
-df = pd.read_excel('C:/Users/melvi/Documents/ReportHistory-26754957.xlsx')
-ds = df[6:8]
-ds.columns = ['Time_o','Position','Symbol',
-           'Type','Volume','Price', 's_l','t_p','Time_c','N_Price','Commision',
-           'Swap','Profit','del']
-ds.pop('del')
-datum = ds.reset_index()
-datum.pop('index')
-
-for i in range(0,len(datum)):
-    date_t = datum['Time_o'][i][:-4]
-    date_time = datetime.strptime(date_t,"%Y.%m.%d %H:%M:%S")
-
-    cargo = malgo(
-        "std",
-        datum['Type'][i],
-        datum['Symbol'][i],
-        float(datum['Volume'][i]),
-        float(datum['Price'][i]),
-        float(datum['N_Price'][i]),
-        500,
-        50,
-        100,
-        float(datum['Swap'][i])/10.0,
-        date_time
-        )
-    p = datum['Profit'][i]
-    c = cargo.profit
-    gc = datum['Symbol'][i]
-    x = c - p
-    print(f'{i}:{gc}{round(x,2)}')
+class malgometer:
+    def __init__(self,login,password,server):
+        self.credentials = {"login":login,"password":password,"server":server,"log":False}
+        asyncio.run(self.mt_log(self.credentials["login"],self.credentials["password"],self.credentials["server"]))
+    async def mt_init(self):
+        try:
+            if mt.initialize() == "true":
+                init  = "True"
+                print("Initialized!")
+            else:
+                init = mt.initialize()
+        except Exception as e:
+            print(e)
+        return init
+    async def login_(self,login,password,server):
+        try:
+            login_state = mt.login(login, password, server)
+        except Exception as e:
+            print(e)
+        return login_state
+    async def mt_log(self,login,password,server):
+        init = asyncio.create_task(self.mt_init())
+        await init
+        state = init._result
+        try:
+            if state == True:
+                log = asyncio.create_task(self.login_(login,password,server))
+                await log
+                if log._result == True:
+                    self.credentials["log"] = log._result
+                else:
+                    raise Exception(f"Trouble connection to {self.credentials['login']}")
+        except Exception as e:
+            print(e)
+        self.mt = mt
--- malgo_says.py
+++ malgo_says.py
@@ -2,53 +2,38 @@ import math
 import time
 import decimal
 import json
-import MetaTrader5 as mt 
+import MetaTrader5 as mt
+from malgo import malgogram
+from malgo import malgometer
 from datetime import datetime
 from datetime import timedelta
+
 
 mt.initialize()
 
 class malgo:
-
-    try:
-        if mt.initialize() == "true":
-            print("Initialized!")
-        else:
-            mt.initialize()
-    except Exception as e:
-        print(e)
-
-    try:
-        if mt.login(login=26754957,password="rsbny8gj", server="MetaQuotes-Demo") == "true":
-            print("loged in")
-        else:
-            mt.login(login=26754957,
-             password="rsbny8gj", server="MetaQuotes-Demo")
-    except Exception as e:
-        print(e)
-
     def __init__(self):
         __account, __open_positon = mt.account_info(), mt.positions_get()
-
-        """leverage = __account[2]
+    def  set_stage(self,__open_positon):
+        """ 
+        leverage = __account[2]
         balance = __account[10]
         limit_orders = __account[3]
         equity = __account[13]
         margin = __account[14]
         margin_free = __account[15]"""
+
         profit = __account[12]
         margin_level = round(__account[16],2)
         name = __account[24]
-        if len(__open_positon) > 0:
-            exchange_rate = __open_positon[0][10]
-            new_exchange_rate = __open_positon[0][13]
-            symbol = __open_positon[0][16]
-
         try:
-            if len(__open_positon) > 0:      
+            if len(__open_positon) > 0:
+                exchange_rate = __open_positon[0][10]
+                new_exchange_rate = __open_positon[0][13]
+                symbol = __open_positon[0][16]  
                 self.get_malgo(name,margin_level,profit,exchange_rate,new_exchange_rate,symbol)
+                self.prepare_trade()
                 self.get_ticks() 
-
             else:
                 self.get_ticks()
         except Exception as e:
@@ -79,16 +64,10 @@ class malgo:
         local_date_time_string = local_date_time.strftime('%a %b %d %H:%M:%S %Y')
 
         return local_date_time_string
+    def prepare_trade(self):
 
-#cargo = malgo("std", "buy", "eur", 0.12, 1.10423, 1.10332, 500, 50)
 if __name__ == "__main__":
     trial = malgo()
-    #get_mal = trial.get_malgo()
-    #time_ = trial.get_local_time()
-    #time_s = time_.strftime('%a %b %d %H:%M:%S %Y')
-    print()
-    #get_mal["Local Time"] = time_s
-    #get_mal_j = get_mal.json()
     with open('c:/Users/melvi/git/malgo/malgo_says.json','a+') as ch:
         for trad in trial.trades:
             json.dump({trad:trial.trades[trad]},ch)
